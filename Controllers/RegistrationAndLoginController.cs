7645c25b93948132c0b27473d040993dcb34b18b
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2024-03-10 17:20:13 +0530

    pushed

---
+++ Controllers/RegistrationAndLoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearningLanguagePlatform.Controllers
+{
+    public class RegistrationAndLoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=RegistrationAndLogin}/{action=Login}/{id?}");
 
 app.Run();
