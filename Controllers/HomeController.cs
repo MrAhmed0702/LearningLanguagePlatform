bb17ce4821af438258a04cc2a569806e6fd016bf
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2024-04-04 04:16:24 +0530

    Updated

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,6 +24,16 @@ namespace LearningLanguagePlatform.Controllers
             return View();
         }
 
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
